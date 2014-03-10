c744eeee14ea8e72ca8490c987f82a63244aea84
11sdw4 <s.sam@hotmail.ca>
2014-03-10 12:48:09 -0400

    Create approachball.c

---
+++ approachball.c
@@ -0,0 +1,10 @@
+when navigate returns a 1 and destination=0,1 or 2
+open gripper
+raise gripper
+line follow until BOTH BUMPERS are hit (range finder to control speed?)
+lower gripper to correct height
+close gripper
+call force sensing error checking 
+if error function returns 1(no error)
+set destination=3
+call navigate

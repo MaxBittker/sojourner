160ecba802873ad3a4249a6eabead628e64813a1
Morgan35 <morgan.williams@queensu.ca>
2014-03-10 13:40:46 -0400

    Update approachBall.c
    pseudocode to more actual code

--- approachBall.c
+++ approachBall.c
@@ -1,13 +1,28 @@
-when navigate returns a 1 and destination=0,1 or 2
-open gripper
-raise gripper
-line follow until BOTH BUMPERS are hit (range finder to control speed?)
-lower gripper to correct height
-close gripper
-call force sensing error checking 
-if error function returns 1(no error)
-set destination=3
-backup (small amount to allow turning)
+//when navigate returns a 1 and destination=0,1 or 2 - incoming (so case check outside)
 
-if error function returns 0 (not gripping ball)
-pause and enter shuffle functions by bluetooth
+boolean approachBall() {
+  
+  grip.write(GRIPopen);
+  tilt.write(TILTup);
+  
+  //line follow until BOTH BUMPERS are hit (range finder to control speed?)
+  
+  boolean ball = ObjGrab();
+   
+  if (ball == true) {
+    destination=3
+    backup (small amount to allow turning)
+  }
+  
+  if (ball == false)
+    pause and enter shuffle functions by bluetooth
+  
+}
+
+
+boolean ObjGrab() {
+  tilt.write(TILThoriz);
+  close gripper etc...
+}
+
+

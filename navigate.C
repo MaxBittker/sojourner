88f196fd5e4d928bd685325bbe706c947e43f41a
MaxBittker <maxbittker@gmail.com>
2014-03-15 12:03:39 -0400

    navigate logic is mostly complete

--- navigate.C
+++ navigate.C
@@ -1,16 +1,67 @@
   
+/* navigate.c */
+
+#include "lineFollow.c"
+#include "approachBall.c"
+#include "approachBucket.c"
+#include "turn180.c"
+
+
 boolean navigate(location start, location destination)  {
   
-  if( startLoc == 4) //begins at center
+  if( start == centre) //begins at center
   {
-    //logic to spin past a certain number of lines, 
+    //TODO logic to spin past a certain number of lines, 
     
-    
+    switch(destination){
+          case zero:
+            while(AnalogRead(left)>LTHRESH && AnalogRead(middle)>MTHRESH && AnalogRead(right)>RTHRESH)
+                {
+                //spin right (possibly with speed as a function of time)
+                }
+                while(AnalogRead(middle)>MTHRESH)
+                  {
+                //spin right slowly 
+                    if(AnalogRead(left)>LTHRESH)
+                        {FIX IT} //went too far :(
+                 }
+            lineFollow();
+            approachBall();
+            return: navigate(destination,bucket);
+            break;
+          case one:
+            lineFollow();
+            approachBall();
+            return: navigate(destination, bucket);
+            break; 
+
+          case two:
+             while(AnalogRead(left)>LTHRESH && AnalogRead(middle)>MTHRESH && AnalogRead(right)>RTHRESH)
+                {
+                //spin left (possibly with speed as a function of time)
+                }
+                while(AnalogRead(middle)>MTHRESH)
+                  {
+                //spin right slowly 
+                    if(AnalogRead(right)>RTHRESH)
+                        {FIX IT} //went too far :(
+                 }
+            lineFollow();
+            approachBall();
+            return: navigate(destination,bucket);
+            break;
+
+          default:
+          //error
+          return false;
+          break;
+
+    }
     lineFollow();
     //passes off control to an approach function
     approachBall();
     
-    navigate(3,destination); //always goes to bucket (3) after it gets the ball
+    navigate(destination,bucket); //always goes to bucket after it gets the ball
     
     return true; //returns to main after 
     
@@ -18,30 +69,71 @@ boolean navigate(location start, location destination)  {
     
   else //6 cases
   {
-    if(startLocation == 3)//if we're at a basket location
+    if(startLocation == bucket)//if we're at a basket location
       {
+        switch(destination){
+          case zero:
+            pivot(-140);
+            braveForray(0); //angle right
+            lineFollow();
+            approachBall();
+            return: navigate(destination,bucket);
+            break;
+          case one:
         
+            turn180();
+            lineFollow();
+            approachBall();
+            return: navigate(destination, bucket);
+            break; 
+
+          case two:
+            pivot(140);
+            braveForray(1); //angle left
+            lineFollow();
+            approachBall();
+
+            return: navigate(destination,bucket);
+            break;
+
+             default:
+          //error
+          return false;
+          break;
+        }
       }
     else //if we're at a ball location
       {
         
          switch (startLocation) {
-      case 0: //bucket is to the left
-      
+      case zero: //bucket is to the right
+          pivot(140);
+          braveForray(1); //(angle left);
+          lineFollow();
+         return: approachBucket();
+
         break;
-      case 1:    //bucket is to the right
-        
+      case two:    //bucket is to the left
+
+         pivot(-140);
+         braveForray(0); //angle right
+         lineFollow();
+
+        return: approachBucket();
+
         break;
-      case 2:    //across from bucket
+      case one:    //across from bucket
+
         turn180();
         lineFollow();
         
         return: approachBucket();
-        
         break;
-     
+
       default:
-        
+          //error
+          return false;
+          break;
         
       }
     

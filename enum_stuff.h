24879b8373e1a5cf191401c13b3c4ce126819f7b
Morgan35 <morgan.williams@queensu.ca>
2014-03-15 13:08:36 -0400

    made bluetooth comms work

---
+++ enum_stuff.h
@@ -0,0 +1,46 @@
+enum location{bucket, zero, one, two, centre, fail}; //counts ccw from bucket  
+
+#include <Arduino.h>
+
+//function to get location from bluetooth
+location getLoc(){
+  
+	int waitTime = 5000;
+	long nowTime = millis();
+  
+	location target = fail;
+	int ch = 'x';
+  
+	while((millis() - nowTime) < waitTime && ch == 'x') {  
+		if(Serial.available()) {
+			ch = Serial.read();
+                        Serial.write(ch);
+                 } 
+	}
+
+        Serial.write(ch);
+    
+	switch(ch) {
+		case '0': target = zero;
+				break;
+		case '1': target = one;
+				break;
+		case '2': target = two;
+				break;
+	} 
+
+	return target;
+  
+}
+
+//doesn't need to be written yet b/c only used if bluetooth fails
+location findLoc(){
+  
+  location target = fail;
+  
+  //drives forward and turns until it finds the blinking target
+  
+  return target;
+  
+}
+
--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 //Group 35  (MB, MW, SW)
 
+#include "enum_stuff.h"
+
 #include <Servo.h>
 Servo pan, grip, tilt;
 
@@ -23,12 +25,12 @@ int rightbump = 3;
 
 //Line Sensor pins
 int left = A0;
-int center = A1;
+int middle = A1;
 int right = A2;
 
 //uncalibrated!
 #define LTHRESH 420
-#define CTHRESH 475
+#define MTHRESH 475
 #define RTHRESH 350
 
 //encoder pins
@@ -43,8 +45,6 @@ int rec = 0;
 int IRpin = A4;
 
 int PWMspeed = 100;  //(40%)  (PWMspeed)
-
-enum location {bucket, zero, one, two, centre, fail}; //counts ccw from bucket  
 
 location currentPos = centre;
 location destination;
@@ -75,14 +75,15 @@ void setup(){
   
   //light sensing pin input
   pinMode(left, INPUT);
-  pinMode(center, INPUT);
+  pinMode(middle, INPUT);
   pinMode(right, INPUT);
   
   //interrupts
   attachInterrupt(0, leftEn, RISING);
   attachInterrupt(1, rightEn, RISING);
   
-  Serial.begin(9600);
+  //Serial.begin(9600);
+  Serial.begin(115200);
   
   waitButton();
   
@@ -96,57 +97,10 @@ void loop(){
     destination = getLoc();
     if(destination == fail)
       destination = findLoc();
-	  
-	  switch(target) {
-		case zero: pivot(45);
-				break;
-		case one: forward(PWMspeed)
-				break;
-		case two: pivot(-45);
-				break;
-	} 
   
     //navigate(currentPos, destination);
 
 }//end loop
-
-//function to get location from bluetooth
-location getLoc(){
-  
-	int waitTime = 5000;
-	long nowTime = millis();
-  
-	location target = fail;
-	int ch;
-  
-	while((millis() - nowTime) < waitTime) {  
-		if(Serial.available())
-			int ch = Serial.read();
-	}
-    
-	switch(ch) {
-		case '0': target = zero;
-				break;
-		case '1': target = one;
-				break;
-		case '2': target = two;
-				break;
-	} 
-  
-	return target;
-  
-}
-
-//doesn't need to be written yet b/c only used if bluetooth fails
-location findLoc(){
-  
-  location target = fail;
-  
-  //drives forward and turns until it finds the blinking target
-  
-  return target;
-  
-}
 
 void pivot(int angle)
 {

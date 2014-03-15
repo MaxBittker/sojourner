c5259fcb35d57d12f68af6720c222aba503386b4
11sdw4 <s.sam@hotmail.ca>
2014-03-15 11:34:41 -0400

    Update approachBucket.c

--- approachBucket.c
+++ approachBucket.c
@@ -2,4 +2,4 @@ when navigate returns a 1 and destination=3
 raise ball to correct height
 line follow until RIGHT BUMPER is hit (range finder to control speed?)
 release ball
-
+back up to allow navigate room to maneuver

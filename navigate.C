54814e0e46de72e9b21f9feed049e8f4ddb3f49b
Morgan35 <morgan.williams@queensu.ca>
2014-03-15 10:49:52 -0400

    Update navigate.C
    added function header

--- navigate.C
+++ navigate.C
@@ -1,47 +1,50 @@
-//recieves (destination,startLocation) from main
-
-if( startLoc == 4) //begins at center
-{
-  //logic to spin past a certain number of lines, 
   
+boolean navigate(location start, location destination)  {
   
-  lineFollow();
-  //passes off control to an approach function
-  approachBall();
-  
-  navigate(3,destination); //always goes to bucket (3) after it gets the ball
-  
-  return true; //returns to main after 
-  
-  }
-  
-else //6 cases
-{
-  if(startLocation == 3)//if we're at a basket location
-    {
-      
-    }
-  else //if we're at a ball location
-    {
-      
-       switch (startLocation) {
-    case 0: //bucket is to the left
+  if( startLoc == 4) //begins at center
+  {
+    //logic to spin past a certain number of lines, 
     
-      break;
-    case 1:    //bucket is to the right
-      
-      break;
-    case 2:    //across from bucket
-      turn180();
-      lineFollow();
-      
-      return: approachBucket();
-      
-      break;
-   
-    default:
-      
-      
+    
+    lineFollow();
+    //passes off control to an approach function
+    approachBall();
+    
+    navigate(3,destination); //always goes to bucket (3) after it gets the ball
+    
+    return true; //returns to main after 
+    
     }
+    
+  else //6 cases
+  {
+    if(startLocation == 3)//if we're at a basket location
+      {
+        
+      }
+    else //if we're at a ball location
+      {
+        
+         switch (startLocation) {
+      case 0: //bucket is to the left
+      
+        break;
+      case 1:    //bucket is to the right
+        
+        break;
+      case 2:    //across from bucket
+        turn180();
+        lineFollow();
+        
+        return: approachBucket();
+        
+        break;
+     
+      default:
+        
+        
+      }
+    
+  }
   
 }

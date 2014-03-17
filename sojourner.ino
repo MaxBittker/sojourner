fdbcd89776751040806ef16600afd3aed32c3cc8
Morgan35 <morgan.williams@queensu.ca>
2014-03-17 14:19:11 -0400

    Made pivot mostly work
    Still needs to be calibrated and maybe use the other encoder as well

--- sojourner.ino
+++ sojourner.ino
@@ -14,7 +14,6 @@ Servo pan, grip, tilt;
 int lec = 0;
 int rec = 0;
 
-
 location currentPos = centre;
 location destination;
 
@@ -47,49 +46,63 @@ void setup(){
   pinMode(middle, INPUT);
   pinMode(right, INPUT);
   
+  //encoder pin assignment
+  pinMode(le, INPUT);
+  pinMode(re, INPUT);
+  
   //interrupts
-  attachInterrupt(0, leftEn, RISING);
-  attachInterrupt(1, rightEn, RISING);
+  //attachInterrupt(11, leftEn, RISING);
+  //attachInterrupt(12, rightEn, RISING);
   
   //Serial.begin(9600);
   Serial.begin(115200);
   
   waitButton();
-  
-  //spin and find target - or take input
-
 }
 
 
 void loop(){
   
-    destination = getLoc();
-    if(destination == fail)
-      destination = findLoc();
-    if(destination !=fail)
-    navigate(currentPos, destination);
+  pivot(90);
+  delay(500);
+  pivot(-90);  //NOT WORKING?????!!!!???
+  delay(500);
+//    destination = getLoc();
+//    if(destination == fail)
+//      destination = findLoc();
+//      
+//    if(destination !=fail)
+//      navigate(currentPos, destination);
 
 }//end loop
 
 void pivot(int angle)
 {
-  analogWrite(E1, PWMspeed);
-  analogWrite(E2, PWMspeed); 
-  
   delay(100);
   
-  if (angle > 0)
-  {
-    digitalWrite(M1, LOW);
-    digitalWrite(M2, HIGH);
-  }  
-  else if (angle < 0)
-  {
-    digitalWrite(M1, HIGH);
-    digitalWrite(M2, LOW);
+  if (angle > 0) {
+      digitalWrite(M1, LOW);
+      digitalWrite(M2, HIGH);
+  }else if (angle < 0) {
+      digitalWrite(M1, HIGH);
+      digitalWrite(M2, LOW);
   }
-
-  delay(10*abs(angle));
+  
+  lec = 0;
+  rec = 0;
+  
+  float ratio = encCounts/(float)turnAngle;
+  
+  analogWrite(E1, PWMspeed);
+  analogWrite(E2, PWMspeed);
+    
+  while(rec < (int)(abs(angle)*ratio)) {    
+    int re_old = digitalRead(re);
+    while(digitalRead(re) == re_old){}
+    rec++;
+  }
+  
+  forward(0);
 }
 
 void forward(int speed) {
@@ -119,12 +132,12 @@ void waitButton(){
 
 
 //  --- FUNCTIONS TO INCREMENT ENCODER  ---
-void leftEn() {
-  lec++;
-}
-
-void rightEn() {
-  rec++;
-}
+//void leftEn() {
+//  lec++;
+//}
+//
+//void rightEn() {
+//  rec++;
+//}
 
 
